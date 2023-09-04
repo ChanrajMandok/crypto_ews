823f3c0f4d302156f5199457bc4e7feeadb43517
ChanrajMandok <89838945+ManrajChandok@users.noreply.github.com>
2023-09-04 08:55:46 +0100

    change to db manager to ensure reminders sent only once

--- binance_ews_app/services/service_binance_db_event_manager.py
+++ binance_ews_app/services/service_binance_db_event_manager.py
@@ -13,18 +13,24 @@ class ServiceBinanceDbEventManager:
         self.__service_send_binance_event_to_ms_teams = ServiceSendModelBinanceEventToMsTeams()
 
     def manage_db(self):
-        now = int(datetime.now().timestamp())*1000  # Current timestamp
+        now = int(datetime.now().timestamp()) * 1000  # Current timestamp
         incomplete_events = ModelBinanceEvent.objects.filter(event_completed=False)
 
-        expired_events = [event for event in incomplete_events if any(int(ts) < now for ts in event.important_dates)]
+        for event in incomplete_events:
+            expired_dates = [int(ts) for ts in event.important_dates if int(ts) < now]
 
-        for event in expired_events:
-            reminder_msg = event.ms_teams_message
-            reminder_msg['title'] = 'REMINDER ' + event.ms_teams_message['title']
-            reminder_msg['sections'][1] = {"activityTitle": f"Priority: {EnumPriority.REMINDER}"}
+            if expired_dates:
+                # Send message to MS Teams for the expired dates
+                reminder_msg = event.ms_teams_message
+                reminder_msg['title'] = 'REMINDER ' + event.ms_teams_message['title']
+                reminder_msg['sections'][1] = {"activityTitle": f"Priority: {EnumPriority.REMINDER.name}"}
+                self.__service_send_binance_event_to_ms_teams.send_message(reminder_msg)
 
-            self.__service_send_binance_event_to_ms_teams.send_message(reminder_msg)
-
-            if all(int(ts)-26000000 < now for ts in event.important_dates):
-                event.event_completed = True 
+                # Pop the expired dates from the event's important_dates list
+                event.important_dates = [ts for ts in event.important_dates if int(ts) not in expired_dates]
                 event.save()
+
+                # Check if all dates are expired, mark the event as completed
+                if all(int(ts)-26000000 < now for ts in event.important_dates):
+                    event.event_completed = True 
+                    event.save()
